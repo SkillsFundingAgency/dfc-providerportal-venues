8a3207ce69acc3563c33046b3c9b092a8b69cf1e
Ian Rose <ian.findacourse@gmail.com>
2018-11-28 10:28:16 +0000

    Support all logging event levels

--- src/Dfc.ProviderPortal.Venues.Models/Helpers/LogHelper.cs
+++ src/Dfc.ProviderPortal.Venues.Models/Helpers/LogHelper.cs
@@ -9,28 +9,60 @@ using Microsoft.Extensions.Logging;
 
 namespace Dfc.ProviderPortal.Venues.Storage
 {
+    /// <summary>
+    /// Temporary implementation of a wrapper so that ILogger can be passed to code common to both Azure
+    /// Functions (which has to use deprecated TraceWriter for logging to work when deployed, ILogger
+    /// doesn't work properly yet) and other code such as WebAPI or MVC projects which use ILogger
+    /// </summary>
     public class LogHelper : ILogger
     {
-        //public class MyTraceWriter : TraceWriter {
-        //    public MyTraceWriter(TraceLevel traceLevel) { }
-        //    public override void Trace(TraceEvent traceEvent) { }
-        //}
-
+        /// <summary>
+        /// Private variables holding passed logger object
+        /// </summary>
         private TraceWriter _tw = null;
         private ILogger _log = null;
 
+        /// <summary>
+        /// Public constructors both require a logger object as an argument
+        /// </summary>
+        /// <param name="log"></param>
+        private LogHelper() { }
         public LogHelper(ILogger log) { _log = log; }
         public LogHelper(TraceWriter tw) { _tw = tw; }
 
+        /// <summary>
+        /// Simple wrapper to log event using logger passed to constructor
+        /// </summary>
         public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter) {
-            if (_log != null)
+
+            // Log event
+            if (_log != null)                                               // Use ILogger as preferable method
                 _log.Log(logLevel, eventId, state, exception, formatter);
-            else if (_tw != null)
-                _tw.Info(formatter.ToString());
+            else if (_tw != null) {                                         // or TraceWriter as a fallback if available
+                switch (logLevel) {
+                    case LogLevel.Information:
+                        _tw.Info(state.ToString()); break;
+                    case LogLevel.Warning:
+                        _tw.Warning(state.ToString()); break;
+                    case LogLevel.Critical:
+                    case LogLevel.Error:
+                        _tw.Error(state.ToString()); break;
+                    //case LogLevel.Trace:
+                    //case LogLevel.Debug:
+                    //case LogLevel.None:
+                    //    break;
+                }
+            }
         }
 
+        /// <summary>
+        /// Required for ILogger implementation - loggers report permanently enabled for now
+        /// </summary>
         public bool IsEnabled(LogLevel logLevel) { return true; }
 
+        /// <summary>
+        /// Required for ILogger implementation
+        /// </summary>
         public IDisposable BeginScope<TState>(TState state) { return null; }
     }
 }
