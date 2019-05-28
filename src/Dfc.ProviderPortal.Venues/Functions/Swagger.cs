5803f5097cb6592b72946a14d35dbdf843e42801
Mahmood Akhtar <mahmood233@hotmail.com>
2019-05-28 16:09:36 +0100

    COUR-778 Adding the swaggering stuff and code tidy and clean.

---
+++ src/Dfc.ProviderPortal.Venues/Functions/Swagger.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Microsoft.OpenApi.Writers;
+using Newtonsoft.Json.Linq;
+using Swashbuckle.AspNetCore.Swagger;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dfc.ProviderPortal.Venues.Functions
+{
+    public static class Swagger
+    {
+        [FunctionName("Swagger")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "swagger.json")] HttpRequest req,
+            ILogger log)
+        {
+            var url = req.GetDisplayUrl();
+            if (req.Path.HasValue && !string.IsNullOrWhiteSpace(req.Path.Value)) url = url.Replace(req.Path, string.Empty);
+            if (req.QueryString.HasValue && !string.IsNullOrWhiteSpace(req.QueryString.Value)) url = url.Replace(req.QueryString.Value, string.Empty);
+            var host = new WebHostBuilder().UseStartup<Startup>().Build();
+            var swaggerProvider = host.Services.GetRequiredService<ISwaggerProvider>();
+            var swagger = swaggerProvider.GetSwagger("v1");
+            swagger.Servers.Add(new OpenApiServer { Url = url });
+
+            using (var writer = new StringWriter(new StringBuilder()))
+            {
+                var jsonWriter = new OpenApiJsonWriter(writer);
+                swagger.SerializeAsV3(jsonWriter);
+                var obj = JObject.Parse(writer.ToString());
+                return new JsonResult(obj);
+            }
+        }
+    }
+}
